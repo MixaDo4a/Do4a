ca6b7f7876b5ce8e444373011d549ed237eb92f8
Михас <mixarules88@gmail.com>
2026-09-23 18:31:12 +1000

    Port full prototype interface and interactions to online app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      { source: "/", destination: "/legacy/index.html" },
+      { source: "/admin", destination: "/legacy/admin.html" },
+      { source: "/product-options.html", destination: "/legacy/product-options.html" },
+      { source: "/product.html", destination: "/legacy/product.html" },
+    ];
+  },
 };
 
 export default nextConfig;
